ed6c1d3b7142158b8033da00298e86b49c658f9b
ZiTingW <wenzt@outlook.com>
2024-10-29 17:57:36 +1100

    Update standardAL.sh

--- scripts/standardAL.sh
+++ scripts/standardAL.sh
@@ -12,7 +12,7 @@ outpath_base='./res/'
 for iexp in '1' '2' '3'
 do	
 	
-	python train.py --expid $iexp --sampling_strategy $isamp --outpath_base $outpath_base --dataset_name $dataset --dataset_path $dataset_path --al_budget "[10000]*10+[50000]*6" --train_eps 130 --lr 0.1 --cls_lr 0.1 --weight_decay 0.0 --milestone '60,80' --early_stop 130 --batchsize_train 64 --grad_accu 4 --batchsize_al_forward 128 --batchsize_evaluation 128 --training_mode 2 --classifier_type "Linear" --classifier_dim '2048,2048,10' --network "res50" --selfmodel_path $selfmodel_path --totfeas_path $feas_path'totfeas.npy' --totlabel_path $feas_path'totlabel_img.npy' --totfeas_test_path $feas_path'totfeas_test.npy' --totlabel_test_path $feas_path'totlabel_img_test.npy'
+	python train.py --expid $iexp --sampling_strategy $isamp --outpath_base $outpath_base --dataset_name $dataset --dataset_path $dataset_path --al_budget "[10000]*10+[50000]*6" --train_eps 50 --lr 0.01 --cls_lr 0.1 --weight_decay 0.0001 --milestone '30,40' --early_stop 50 --batchsize_train 128 --grad_accu 1 --batchsize_al_forward 512 --batchsize_evaluation 512 --training_mode 2 --classifier_type "Linear" --classifier_dim '2048,2048,1000' --network "res50" --selfmodel_path $selfmodel_path --totfeas_path $feas_path'totfeas.npy' --totlabel_path $feas_path'totlabel_img.npy' --totfeas_test_path $feas_path'totfeas_test.npy' --totlabel_test_path $feas_path'totlabel_img_test.npy'
 
 done
 		
